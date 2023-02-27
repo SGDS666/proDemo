c3ca585c9b423c6bf6d9442adc9a31af4033adbf
zhangsen <1226931269@qq.com>
2023-02-27 12:53:41 +0800

    remove

--- myapp/src/hooks/useLink.ts
+++
@@ -1,9 +0,0 @@
-import { history } from "@umijs/max"
-import { useCallback } from "react"
-
-export const useLink = () => {
-    const linkto = useCallback((href:string) => {
-        history.push(href)
-    }, [])
-    return linkto
-}
